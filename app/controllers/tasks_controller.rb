a6f8221185703b85f3a1d0d7484905fb73ff7c04
EC2 Default User <ec2-user@ip-172-31-18-2.ap-northeast-1.compute.internal>
2021-04-03 05:32:46 +0000

    C9 MVC 完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,55 @@
 class TasksController < ApplicationController
-  def new
+
+  def index
+    @user = User.find(params[:user_id])
+    @tasks = @user.tasks
   end
+  
+  def show
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.find_by(id: params[:id])
+  end
+  
+  def new
+    @user = User.find(params[:user_id])
+    @task = Task.new
+  end
+  
+  def create
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.build(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to user_tasks_url @user
+    else
+      render :new
+    end
+  end
+  
+  def edit
+     @user = User.find(params[:user_id])
+     #<!--タスクの値を取ってくる-->
+     @task = @user.tasks.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.find_by(id: params[:id])
+    @task.update_attributes(task_params)
+    redirect_to user_tasks_url(@user, @task)
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    @task = @user.tasks.find_by(id: params[:id])
+    @task.destroy
+    redirect_to user_tasks_url @user
+  end
+  
+  private
+
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/new', to: 'tasks#new'
-
   root 'static_pages#top'
   get '/signup', to: 'users#new'
-  
+
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
